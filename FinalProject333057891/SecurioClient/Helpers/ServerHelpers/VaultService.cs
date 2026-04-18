88cb222cc835a52d4a953448fe97bfa423f4bd5a
yoav-home <yoavip19@gmail.com>
2026-04-18 11:50:11 +0300

    add password pre-test

--- FinalProject333057891/SecurioClient/Helpers/ServerHelpers/VaultService.cs
+++ FinalProject333057891/SecurioClient/Helpers/ServerHelpers/VaultService.cs
@@ -1,6 +1,15 @@
-﻿using SecurioModels;
-using SecurioModels.DataTransferObjects;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SecurioModels.DataTransferObjects;
 
 namespace SecurioClient.Helpers.ServerHelpers
 {
