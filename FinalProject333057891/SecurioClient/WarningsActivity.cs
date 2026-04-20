0b18f3539622ccafc2c181c53b32480d6f20e008
yoav-home <yoavip19@gmail.com>
2026-04-20 12:57:58 +0300

    display at oncreate and onresume

--- FinalProject333057891/SecurioClient/WarningsActivity.cs
+++ FinalProject333057891/SecurioClient/WarningsActivity.cs
@@ -35,6 +35,7 @@ namespace SecurioClient
             InitializeViews();
             SetupBottomNavFragment(savedInstanceState);
             SetupViewAllButtons();
+            DisplayWarnings();
         }
 
         protected override void OnResume()
